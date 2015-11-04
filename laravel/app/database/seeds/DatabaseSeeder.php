e417119b9a830b7a6ae435206068b3cf131acde0
Pradeep Kumar <webreinvent@gmail.com>
2015-11-05 01:15:41 +0530

    - Bug Fix in seeder, it was not running on cpanel based hosting

--- laravel/app/database/seeds/DatabaseSeeder.php
+++ laravel/app/database/seeds/DatabaseSeeder.php
@@ -14,12 +14,19 @@ class DatabaseSeeder extends Seeder {
 		$seed_path = app_path()."/database/seeds";
 		$scan_seeds = scan_dir_paths($seed_path);
 
+
+
 		foreach($scan_seeds as $seed)
 		{
 
 			$seed = str_replace(".php", "", $seed);
 			$seed = str_replace("$seed_path", "", $seed);
 			$seed = str_replace('\\', '', $seed);
+
+
+			if (strpos($seed,'/') !== false) {
+				$seed = str_replace('/', '', $seed);
+			}
 
 			if($seed == 'DatabaseSeeder')
 			{
