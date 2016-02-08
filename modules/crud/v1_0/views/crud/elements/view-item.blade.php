b999911ce244ff0cb32ce74e5e2d813e2ab13383
Pradeep Kumar <webreinvent@gmail.com>
2016-02-08 07:54:01 +0530

    Crud view-item bug fix

--- modules/crud/v1_0/views/crud/elements/view-item.blade.php
+++ modules/crud/v1_0/views/crud/elements/view-item.blade.php
@@ -2,11 +2,11 @@
     <table class="table table-bordered table-striped table-condensed">
         @foreach($data as $key => $value )
             <?php
-            if ($key == 'created_by' && !empty($value)) {
+            if ($key == 'created_by' && is_object($value)) {
                 $value = $value->name;
-            } else if ($key == 'modified_by' && !empty($value)) {
+            } else if ($key == 'modified_by' && is_object($value)) {
                 $value = $value->name;
-            } else if ($key == 'deleted_by' && !empty($value)) {
+            } else if ($key == 'deleted_by' && is_object($value)) {
                 $value = $value->name;
             }
             ?>
